455343db03e7005ddf6850487670d25388182986
Ritme54 <tmdwns5456@naver.com>
2025-07-17 15:44:08 +0900

    2025.07.17(데크)

---
+++ Program/Class3th(Circe Linked List)/Class3th(Circe Linked List).cpp
@@ -0,0 +1,183 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+template<typename T>
+class CircleList
+{
+public:
+	CircleList()
+	{
+		size = 0;
+
+		head = nullptr;
+		cout << "초기화" << endl;
+
+	}
+
+	void push_back(T data)
+	{
+
+		Node* newNode = new Node;
+		newNode->data = data;
+
+
+		if (head == nullptr)
+		{
+			head = newNode; //리스트가 비여있을때.
+			newNode->next = newNode;
+
+		}
+		else
+		{
+			newNode->next = head->next;
+			head->next = newNode;
+			head = newNode;
+		}
+		size++;
+	}
+
+	void push_front(T data)
+	{
+		Node* newNode = new Node;
+		newNode->data = data;
+
+		if (head == nullptr)
+		{
+			newNode->next = newNode;
+			head = newNode; //리스트가 비여있을때.
+		}
+		else
+		{
+			newNode->next = head->next;
+			head->next = newNode;
+		}
+		size++;
+	}
+
+	void pop_front()
+	{
+		if (head == nullptr)
+		{
+			cout << "List is empty" << endl;
+			return;
+		}
+		Node* delNode = head->next;
+
+		if (head->next == head)
+		{
+			delete head;
+			head = nullptr;
+			size--;
+		}
+		else
+		{
+			head->next = delNode->next;
+			delete delNode;
+			size--;
+		}
+
+	}
+
+	void pop_back()
+	{
+		Node* delNoed = head;
+		Node* currentNode = head;
+
+		if (head == nullptr)
+		{
+			cout << "List is empty" << endl;
+			return;
+		}
+		else
+		{
+			if (head == head->next)
+			{
+				head = nullptr;
+			}
+			else
+			{
+				for (int i = 0; i < size - 1; i++)
+				{
+					currentNode = currentNode->next;
+				}
+				currentNode->next = head->next;
+				head = currentNode;
+			}
+			delete delNoed;
+
+			size--;
+		}
+	}
+
+	void printList() {
+		if (head == nullptr) {
+			std::cout << "리스트가 비어 있습니다." << std::endl;
+
+		}
+		else
+		{
+
+			Node* current = head->next; // 첫 번째 노드부터 시작
+			do {
+				std::cout << current->data << " ";
+				current = current->next;
+			} while (current != head->next);
+			std::cout << std::endl;
+		}
+	}
+
+
+	bool empty()
+	{
+		return (head == nullptr);
+	}
+
+	~CircleList()
+	{
+
+		while (head != nullptr)
+		{
+			pop_front();
+		}
+		cout << "삭제" << endl;
+
+
+	}
+
+private:
+	struct Node
+	{
+		T data;
+		Node* next;
+	};
+
+	Node* head;
+	int size;
+
+};
+
+
+int main()
+{
+	CircleList<int> list;
+	list.push_back(10);
+	list.push_back(20);
+	list.push_back(30);
+	list.push_front(5);
+	list.push_front(1);
+	list.pop_front();
+	list.pop_front();
+	list.pop_front();
+
+	 list.pop_back();
+	 
+	 cout << list.empty() << endl;
+	 
+	 list.printList();
+
+
+
+	return 0;
+}
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -4,136 +4,92 @@ using namespace std;
 
 
 template<typename T>
-class CircleList
+class Vector
 {
 public:
-	CircleList()
+	Vector()
 	{
 		size = 0;
-
-		head = nullptr;
+		capacity = 0;
+		container = nullptr;
 		cout << "초기화" << endl;
+	};
+	~Vector()
+	{
+		if (container != nullptr)
+		{
+			delete[]container;
+		}
+	}
+
+
+	void resize(int newSize)
+	{
+		//1.capacity에 새로운 size값을 저장한다.
+		capacity = newSize;
+
+		//2.새로운 포인터 변수를 생성해서 새롭게 만들어진 메모리 공간을 가리키도록 한다.
+		T*pointer = new T[capacity]
+		//3.새로운 메모리 공간의 값을 초기화(쓰래기값제거)
+			for (int i = 0; i < capacity; i++)
+			{
+				pointer[i] = NULL;
+			}
+		//4.기존 배열에 있는 값을 복사해서 새로운 배열에 넣어준다.
+			for (int i = 0; i < size; i++)
+			{
+				pointer[i] = container[i];
+			}
+		//5.기존 배열의 메모리를 해제한다.(Nullptr이 아닐때 해제한다!)
+			if (container !=nullptr)
+			{
+				delete[] container
+			}
+		//6.기존 베열을 가리키던 포인터 변수의 값을 새로운 배열의 시작 주소로 가리킨다.
+			container = pointer;
+			capacity = newcapacity;
 		
 	}
 
 	void push_back(T data)
 	{
-
-		Node* newNode = new Node;
-		newNode->data = data;
-		
-
-		if (head == nullptr)
+		//데이터가 존재하지 않을때= 하나를 우선 만든다.(size=0)
+		//값을 하나 더 넣는다-> 사이즈와 값 크기가  증가한다.
+		if (size== capacity)
 		{
-			head = newNode; //리스트가 비여있을때.
-			newNode->next = newNode;
+			int newcapacity = (capacity == 0) ? 1 : capacity * 2;
+			resize(newcapacity);
+		}
+		container[size] = value;
+		size++
 
-		}
-		else
-		{
-			newNode->next = head->next;
-			head->next = newNode;
-			head = newNode;
-		}
-		size++;
 	}
 
-	void push_front(T data)
-	{
-		Node* newNode = new Node;
-		newNode->data = data;
-
-		if (head == nullptr)
-		{
-			newNode->next = newNode;
-			head = newNode; //리스트가 비여있을때.
+	void display() const {
+		std::cout << "Vector 내용: [";
+		for (size_t i = 0; i < size; ++i) {
+			std::cout << container[i] << (i == size - 1 ? "" : ", ");
 		}
-		else
-		{
-			newNode->next = head->next;
-			head->next = newNode;
-		}
-		size++;
-	}
-
-	void pop_front()
-	{
-		if (head == nullptr)
-		{
-			cout << "List is empty" << endl;
-			return;
-		}
-		Node* delNode = head->next;
-
-		
-		if(head->next==head)
-		{
-			head = nullptr;
-		}
-		else
-		{
-			head->next = delNode->next;
-		}
-		delete delNode;
-		size--;
-	}
-
-
-	void printList() {
-		if (head == nullptr) {
-			std::cout << "리스트가 비어 있습니다." << std::endl;
-			return;
-		}
-		Node* current = head->next; // 첫 번째 노드부터 시작
-		do {
-			std::cout << current->data << " ";
-			current = current->next;
-		} while (current != head->next);
-		std::cout << std::endl;
-	}
-
-
-	bool empty()
-	{
-		return (head == nullptr);
-	}
-
-	~CircleList()
-	{
-	
-		cout << "삭제" << endl;
-
-
+		std::cout << "], 크기: " << size << ", 용량: " << capacity << std::endl;
 	}
 
 private:
-	struct Node
-	{
-		T data;
-		Node* next;
-	};
+	int size; //사이즈
+	int capacity;//크기
+	T* container;//컨테이너
 
-Node* head;
-int size;
+
 
 };
 
 
 int main()
 {
-	CircleList<int> list;
+	Vector<int>list;
+
 	list.push_back(10);
-	list.push_back(20);
-	list.push_back(30);
-	list.push_front(5);
-	list.push_front(1);
-	list.pop_front();
-
-	cout << list.empty() << endl;
-
-	list.printList();
-
-
+	list.display();
+		
 
 	return 0;
 }
