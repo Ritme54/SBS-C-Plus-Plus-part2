97b98d2a0edfd89001193fccd56adff39c8de396
Ritme54 <tmdwns5456@naver.com>
2025-07-18 15:43:54 +0900

    2025.07.18(String)

---
+++ Program/Class4th(vector)/Class4th(vector).cpp
@@ -0,0 +1,134 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+template<typename T>
+class Vector
+{
+public:
+	Vector()
+	{
+		count = 0;
+		capacity = 0;
+		container = nullptr;
+		cout << "초기화" << endl;
+	};
+	~Vector()
+	{
+		if (container != nullptr)
+		{
+			delete[]container;
+		}
+	}
+
+
+	void resize(int newSize)
+	{
+		//1.capacity에 새로운 size값을 저장한다.
+		capacity = newSize;
+
+		//2.새로운 포인터 변수를 생성해서 새롭게 만들어진 메모리 공간을 가리키도록 한다.
+		T* pointer = new T[capacity];
+		//3.새로운 메모리 공간의 값을 초기화(쓰래기값제거)
+		for (int i = 0; i < capacity; i++)
+		{
+			pointer[i] = NULL;
+		}
+		//4.기존 배열에 있는 값을 복사해서 새로운 배열에 넣어준다.
+		for (int i = 0; i < count; i++)
+		{
+			pointer[i] = container[i];
+		}
+		//5.기존 배열의 메모리를 해제한다.(Nullptr이 아닐때 해제한다!)
+		if (container != nullptr)
+		{
+			delete[] container;
+		}
+		//6.기존 베열을 가리키던 포인터 변수의 값을 새로운 배열의 시작 주소로 가리킨다.
+		container = pointer;
+		capacity = newSize;
+
+	}
+
+	void push_back(T data)
+	{
+		if (count == capacity)
+		{
+			int newSize = (capacity == 0) ? 1 : capacity * 2;
+			resize(newSize);
+		}
+		container[count] = data;
+		count++;
+
+	}
+
+	void display() const {
+		std::cout << "Vector 내용: [";
+		for (size_t i = 0; i < count; ++i) {
+			std::cout << container[i] << (i == count - 1 ? "" : ", ");
+		}
+		std::cout << "], 크기: " << count << ", 용량: " << capacity << std::endl;
+	}
+
+	T& operator[](size_t index)
+	{
+		if (index >= count)
+		{
+			cout << "Index out of bounds" << endl;
+		}
+		return container[index];
+
+	}
+	const int& size()
+	{
+		return count;
+	}
+
+	void pop_back()
+	{
+		if (count <= 0)
+		{
+			cout << "vector is empty" << endl;
+
+		}
+		else
+		{
+			container[--count] = NULL;
+		}
+		return;
+	}
+
+	//null로 초기화
+
+
+
+
+
+private:
+	int count; //사이즈
+	int capacity;//크기
+	T* container;//컨테이너
+};
+
+
+int main()
+{
+	Vector<int>vector;
+
+	vector.push_back(10);
+	vector.push_back(20);
+	vector.push_back(30);
+	vector.push_back(40);
+	vector.push_back(50);
+	vector.pop_back();
+	vector.display();
+	cout << vector[0] << endl;
+
+	for (int i = 0; i < vector.size(); i++)
+	{
+		cout << vector[i] << endl;
+	}
+
+	return 0;
+}
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -1,95 +1,72 @@
 ﻿#include <iostream>
+#include <cstring>
 
 using namespace std;
 
-
-template<typename T>
-class Vector
+class String
 {
 public:
-	Vector()
+	String()
 	{
 		size = 0;
-		capacity = 0;
-		container = nullptr;
-		cout << "초기화" << endl;
-	};
-	~Vector()
-	{
-		if (container != nullptr)
-		{
-			delete[]container;
-		}
+		pointer = nullptr;
+
+
 	}
 
-
-	void resize(int newSize)
+	void operator=(const char* word)
 	{
-		//1.capacity에 새로운 size값을 저장한다.
-		capacity = newSize;
-
-		//2.새로운 포인터 변수를 생성해서 새롭게 만들어진 메모리 공간을 가리키도록 한다.
-		T*pointer = new T[capacity]
-		//3.새로운 메모리 공간의 값을 초기화(쓰래기값제거)
-			for (int i = 0; i < capacity; i++)
-			{
-				pointer[i] = NULL;
-			}
-		//4.기존 배열에 있는 값을 복사해서 새로운 배열에 넣어준다.
+		size = strlen(word) + 1;
+		if (pointer == nullptr)
+		{
+			pointer = new char[size];
 			for (int i = 0; i < size; i++)
 			{
-				pointer[i] = container[i];
+				pointer[i] = word[i];
 			}
-		//5.기존 배열의 메모리를 해제한다.(Nullptr이 아닐때 해제한다!)
-			if (container !=nullptr)
-			{
-				delete[] container
-			}
-		//6.기존 베열을 가리키던 포인터 변수의 값을 새로운 배열의 시작 주소로 가리킨다.
-			container = pointer;
-			capacity = newcapacity;
-		
-	}
 
-	void push_back(T data)
-	{
-		//데이터가 존재하지 않을때= 하나를 우선 만든다.(size=0)
-		//값을 하나 더 넣는다-> 사이즈와 값 크기가  증가한다.
-		if (size== capacity)
+		}
+		else
 		{
-			int newcapacity = (capacity == 0) ? 1 : capacity * 2;
-			resize(newcapacity);
+			char* container = new char[size];
+			for (int i = 0; i < size; i++)
+			{
+				container[i] = word[i];
+			}
+			delete[] pointer;
+			pointer = container;
 		}
-		container[size] = value;
-		size++
+	}
+
+
+	const int& length()
+	{
 
 	}
 
-	void display() const {
-		std::cout << "Vector 내용: [";
-		for (size_t i = 0; i < size; ++i) {
-			std::cout << container[i] << (i == size - 1 ? "" : ", ");
-		}
-		std::cout << "], 크기: " << size << ", 용량: " << capacity << std::endl;
+
+	~String()
+	{
+		delete[] pointer;
 	}
 
 private:
-	int size; //사이즈
-	int capacity;//크기
-	T* container;//컨테이너
-
+	int size;
+	char* pointer;
 
 
 };
 
 
+
 int main()
 {
-	Vector<int>list;
+	//문자열의 길이를 반환하는 
+	String message;
+	message = "League of Legend";
 
-	list.push_back(10);
-	list.display();
-		
+	message = "Adidas";
 
-	return 0;
+
+
 }
